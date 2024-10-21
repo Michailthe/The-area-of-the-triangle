7b81dd706a4df07ab152ae42e8fb8da9f560662b
Michailthe <112062292+Michailthe@users.noreply.github.com>
2024-10-21 23:22:57 +0300

    Добавьте файлы проекта.

---
+++ Площадь треугольника/Площадь треугольника.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <vector>
+using namespace std;
+int main() {
+
+    int footing, height, square;
+    cout << "Enter the base of the triangle" << endl;
+    cin >> footing;
+    cout << "Enter the height of the triangle" << endl;
+    cin >> height;
+
+    if (height < 0)
+    {
+        cout << "The height must be a positive number" << endl;
+    }
+
+    square = (footing * height) / 2;
+    cout << "The area of the triangle" << square << endl;
+}
